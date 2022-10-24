aded5eeca49fbea18800d90555eeee57ca48523b
Robson Harrison <97robsonharrison@gmail.com>
2022-10-24 12:27:01 +0100

    New file added new.py

---
+++ new.py
@@ -0,0 +1,11 @@
+def newSub():
+    print("Hello")
+    print("The Silence")
+    print("Will Fall")
+    print("When The Question")
+    print("Is Asked")
+
+newSub()
+
+import random
+print (random.randint (0,50))
